9bc23e5caf37f4d6b69cd0a6b7dd8d7687087009
Arbab Kareem <144175048+itsarbab0@users.noreply.github.com>
2025-03-10 23:59:48 +0500

    Time: 0 ms (100.00%) | Memory: 11.7 MB (41.31%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,33 +1,15 @@
-#include<limits>
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        if(nums.size() == 0){
-            return 0;
-        }
-        bool flag = false;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == val){
-                nums[i] = INT_MAX;
-                flag = true;
+        int k = 0;  
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];  
+                k++;  
             }
         }
-
-        for(int i = 0; i < nums.size(); i++){
-            for (int j = i + 1; j < nums.size(); j++){
-                if(nums[i] >= nums[j]){
-                    swap(nums[i], nums[j]);
-                }
-            }
-        }
-
-        if(!flag){
-            return nums.size();
-        }
-        int k = 0;
-        while(nums[k] != INT_MAX){
-            k++;
-        }
-        return k;
+        
+        return k;  
     }
 };
